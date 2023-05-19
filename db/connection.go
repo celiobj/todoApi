d5d9cb47a1f9c8164a724cb0ee6948be08a846d2
Celio <celio@gft.com>
2023-05-19 17:18:48 -0300

    Inclusão da entidade e dos CRUD

--- db/connection.go
+++ db/connection.go
@@ -1,1 +1,26 @@
 package db
+
+import (
+	"database/sql"
+	"fmt"
+
+	configs "github.com/celiobj/todoApi/Configs"
+
+	_ "github.com/lib/pq"
+)
+
+func OpenConnection() (*sql.DB, error) {
+	conf := configs.GetDb()
+
+	sc := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode= disable",
+		conf.Host, conf.Port, conf.User, conf.Pass, conf.Database)
+
+	conn, err := sql.Open("postgres", sc)
+	if err != nil {
+		panic(err)
+	}
+
+	err = conn.Ping()
+
+	return conn, err
+}
